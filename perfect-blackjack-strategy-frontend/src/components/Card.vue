ca1ac446cb8b62c47aa441f5301768ca7a617b91
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-10-14 23:05:22 -0600

    Configured tailwind and basic card components

---
+++ perfect-blackjack-strategy-frontend/src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card-body">
+    <div class="card-id">
+      <span class="card-number">7</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+    </div>
+    <div class="card-suit-emblems-body">
+      <span class="suit-emblem">&lt;&gt;</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+    </div>
+    <div class="card-id-bottom">
+      <span class="card-number">7</span>
+      <span class="suit-emblem">&lt;&gt;</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: { }
+})
+export default class Card extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
---
+++ perfect-blackjack-strategy-frontend/src/components/Deck.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Card />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Card from './Card.vue'
+
+@Options({
+  components: {
+    Card
+  }
+})
+export default class Deck extends Vue {}
+</script>
+
+<style lang="scss">
+</style>
